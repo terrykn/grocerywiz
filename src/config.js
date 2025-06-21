041bfcab9c871e57a812df1f6e6c6b84cf4a2263
Terry Nguyen <terryknguyen@gmail.com>
2025-06-20 22:37:39 -0400

    deleted commits

---
+++ src/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: post.env.REACT_APP_FIREBASE_API,
+  authDomain: "grocerywiz-f2aeb.firebaseapp.com",
+  projectId: "grocerywiz-f2aeb",
+  storageBucket: "grocerywiz-f2aeb.firebasestorage.app"
+};
+// hi
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+export { auth, provider, db };
